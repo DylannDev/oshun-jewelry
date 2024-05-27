2fde9e560205a8ed836a1d7229f9607f4e1e6349
dylann97 <d.xavero@hotmail.com>
2024-05-27 07:52:47 +0200

    🚧   F02: List > Fetch product Categories

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -2,14 +2,28 @@
 import BannerList from "@/components/BannerList";
 import Filter from "@/components/Filter";
 import ProductList from "@/components/ProductList";
+import { wixClientServer } from "@/lib/wixClientServer";
+import { Suspense } from "react";
 
-const ListPage = () => {
+const ListPage = async ({ searchParams }: { searchParams: any }) => {
+  const wixClient = await wixClientServer();
+  const category = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
+
   return (
     <>
       <BannerList />
       <Filter />
       <h1 className="mt-12 mb-8 text-xl font-semibold ">Nos Bayas</h1>
-      <ProductList />
+      <Suspense fallback="Chargement...">
+        <ProductList
+          categoryId={
+            category.collection?._id || "00000000-000000-000000-000000000001"
+          }
+          searchParams={searchParams}
+        />
+      </Suspense>
     </>
   );
 };
