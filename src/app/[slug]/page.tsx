45439e97ea87b2aef33e66877ce24347daa8792f
dylann97 <d.xavero@hotmail.com>
2024-05-27 07:53:40 +0200

    🚧   F02: Slug > Fetch a single product from the backend

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,32 +1,53 @@
 import ProductImages from "@/components/ProductImages";
 import ProductPageOptions from "@/components/ProductPageOptions";
+import { wixClientServer } from "@/lib/wixClientServer";
+import { notFound } from "next/navigation";
 
-const SinglePage = () => {
+type SinglePageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+const SinglePage = async ({ params }: SinglePageProps) => {
+  const wixClient = await wixClientServer();
+  const products = await wixClient.products
+    .queryProducts()
+    .eq("slug", params.slug)
+    .find();
+
+  if (!products.items[0]) {
+    return notFound();
+  }
+
+  const product = products.items[0];
+
   return (
     <div className="relative flex flex-col lg:flex-row gap-16">
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max ">
-        <ProductImages />
+        <ProductImages images={product.media?.items} />
       </div>
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <h1 className="text-4xl font-medium">Le Pinky Nefertiti Kama</h1>
-
-        {/* <div className="h-[2px] bg-gray-100" /> */}
-        <div className="flex items-center gap-2">
-          <h3 className="text-lg text-gray-500 line-through ">49€</h3>
-          <h2 className="font-medium text-2xl ">29€</h2>
-        </div>
+        <h1 className="text-4xl font-medium">{product.name}</h1>
+        {product.price?.price === product.price?.discountedPrice ? (
+          <h2 className="font-medium text-2xl ">€{product.price?.price} </h2>
+        ) : (
+          <div className="flex items-center gap-2">
+            <h3 className="text-lg text-gray-500 line-through ">
+              €{product.price?.price}
+            </h3>
+            <h2 className="font-medium text-2xl ">
+              €{product.price?.discountedPrice}{" "}
+            </h2>
+          </div>
+        )}
         <div className="h-[2px] bg-gray-100" />
 
         <ProductPageOptions />
         <div className="h-[2px] bg-gray-100" />
         <div className="">
           <h4 className="text-xs uppercase font-bold mb-4">Description</h4>
-          <p className="text-gray-500 text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-            quibusdam animi dolor dolores, ipsam deserunt aliquid minus
-            cupiditate harum repellat, earum inventore? Beatae assumenda dicta,
-            veniam cupiditate officia ipsa officiis.
-          </p>
+          <p className="text-gray-500 text-sm">{product.description}</p>
         </div>
       </div>
     </div>
