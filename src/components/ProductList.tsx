d29a3256bb82c86fa02d57e1b9aa374522d8db5a
dylann97 <d.xavero@hotmail.com>
2024-06-01 09:48:19 +0200

    ✨ F03: ProductList > Add logic to to sort the products by price or name

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -7,24 +7,43 @@ const PRODUCT_PER_PAGE = 20;
 type ProductListProps = {
   categoryId: string;
   limit?: number;
-  searchParams?: any;
+  searchParams?: { name?: string; sort?: string };
 };
 
 const ProductList = async ({
   categoryId,
   limit,
-  searchParams,
+  searchParams = {},
 }: ProductListProps) => {
   const wixClient = await wixClientServer();
-  const products = await wixClient.products
+  const productQuery = await wixClient.products
     .queryProducts()
     .eq("collectionIds", categoryId)
-    .limit(limit || PRODUCT_PER_PAGE)
-    .find();
+    .limit(limit || PRODUCT_PER_PAGE);
+
+  // Trier les produit par ordre croissant ou décroissant
+  const sortType = searchParams.sort?.toLowerCase();
+  const sortedByPriceQuery =
+    sortType === "descending"
+      ? productQuery.descending("price")
+      : sortType === "ascending"
+      ? productQuery.ascending("price")
+      : productQuery;
+
+  const products = await sortedByPriceQuery.find();
+
+  // Trier les produits par catégorie
+  const filteredProducts = products.items.filter((product: products.Product) =>
+    product.name
+      ?.toLowerCase()
+      .includes((searchParams?.name || "").toLowerCase())
+  );
+
+  // console.log(products.items[11].productOptions[0].choices);
 
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-8">
-      {products.items.map((product: products.Product) => (
+      {filteredProducts.map((product: products.Product) => (
         <ProductCard key={product._id} product={product} />
       ))}
     </div>
