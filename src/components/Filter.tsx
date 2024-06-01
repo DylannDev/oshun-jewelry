d8d0798a97fd562302648e9e631b4dc2a1b7de91
dylann97 <d.xavero@hotmail.com>
2024-06-01 09:46:48 +0200

    ✨ F03: Filter > Add logic with the params in order to get the option selected in the URL

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,65 +1,64 @@
-const Filter = () => {
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+
+const Filter = ({}) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    const params = new URLSearchParams(searchParams.toString());
+    if (value) {
+      params.set(name, value);
+    } else {
+      params.delete(name);
+    }
+    router.replace(`${pathname}?${params.toString()}`);
+  };
   return (
     <div className="mt-12 flex justify-between">
       <div className="flex gap-6 flex-wrap">
         <select
-          name="type"
+          name="size"
           id=""
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200 outline-none"
-        >
-          <option value="">Type</option>
-          <option value="Baya">Baya</option>
-          <option value="Chaîne de Pied">Chaîne de Pied</option>
-        </select>
-        <input
-          type="text"
-          name="min"
-          placeholder="Prix min"
-          className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-200 outline-none"
-        />
-        <input
-          type="text"
-          name="max"
-          placeholder="Prix max"
-          className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-gray-200 outline-none"
-        />
-        <select
-          name="type"
-          id=""
-          className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200 outline-none"
+          onChange={handleFilterChange}
         >
           <option value="">Taille</option>
-          <option value="Baya">Baya</option>
-          <option value="Chaîne de Pied">Chaîne de Pied</option>
+          <option value="XS">XS</option>
+          <option value="S">S</option>
+          <option value="M">M</option>
+          <option value="L">L</option>
         </select>
         <select
-          name="type"
+          name="cat"
           id=""
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200 outline-none"
+          onChange={handleFilterChange}
         >
-          <option value="">Couleur</option>
-          <option value="Baya">Baya</option>
-          <option value="Chaîne de Pied">Chaîne de Pied</option>
-        </select>
-        <select
-          name="type"
-          id=""
-          className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200 outline-none"
-        >
-          <option value="">Tous les filtres</option>
-          <option value="Baya">Baya</option>
-          <option value="Chaîne de Pied">Chaîne de Pied</option>
+          <option value="">Catégories</option>
+          <option value="chaîne-de-taille">Chaînes de Taille</option>
+          <option value="Chaîne de Pied">Chaînes de Pied</option>
+          <option value="Boucles d'oreilles">Boucles d'oreilles</option>
+          <option value="Bracelets">Bracelets</option>
+          <option value="Colliers">Colliers</option>
+          <option value="Bagues">Bagues</option>
         </select>
       </div>
       <div className="flex gap-6 h-fit">
         <select
-          name="type"
+          name="sort"
           id=""
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-gray-200 outline-none"
+          onChange={handleFilterChange}
         >
           <option value="">Trier par</option>
-          <option value="Prix croissant">Prix croissant</option>
-          <option value="Prix décroissant">Prix décroissant</option>
+          <option value="ascending">Prix croissant</option>
+          <option value="descending">Prix décroissant</option>
         </select>
       </div>
     </div>
