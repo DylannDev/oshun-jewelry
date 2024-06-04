8a525f219b26577a1901b18b6211678a99e2e3c8
dylann97 <d.xavero@hotmail.com>
2024-06-04 19:27:01 +0200

    🔧  F03: SinglePage > install DOMPurify to prevent XSS attacks (dangerouslySetInnerHTML)

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,7 +2,7 @@ import ProductImages from "@/components/ProductImages";
 import ProductPageOptions from "@/components/ProductPageOptions";
 import { wixClientServer } from "@/lib/wixClientServer";
 import { notFound } from "next/navigation";
-import { products } from "@wix/stores";
+import DOMPurify from "isomorphic-dompurify";
 
 type SinglePageProps = {
   params: {
@@ -22,6 +22,9 @@ const SinglePage = async ({ params }: SinglePageProps) => {
   }
 
   const product = products.items[0];
+  const sanitizedDescription = product.description
+    ? DOMPurify.sanitize(product.description)
+    : "";
 
   return (
     <div className="relative flex flex-col lg:flex-row gap-16">
@@ -47,7 +50,12 @@ const SinglePage = async ({ params }: SinglePageProps) => {
         <div className="h-[2px] bg-gray-100" />
         <div className="">
           <h4 className="text-xs uppercase font-bold mb-4">Description</h4>
-          <p className="text-gray-500 text-sm">{product.description}</p>
+          <div
+            className="text-gray-500 text-sm"
+            dangerouslySetInnerHTML={{
+              __html: DOMPurify.sanitize(sanitizedDescription),
+            }}
+          ></div>
         </div>
       </div>
     </div>
