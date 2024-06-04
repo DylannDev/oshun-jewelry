70b7e3033fd42b7d812c70d3500d1d37e437f9db
dylann97 <d.xavero@hotmail.com>
2024-06-04 19:30:36 +0200

    🚧  F03: Button > Possibilty to use the component like a button or a link

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,46 @@
 import Link from "next/link";
+import React from "react";
 
 type ColorOption = "white" | "black" | "green" | "red";
 type WidthOption = "large" | "normal";
 
 type ButtonProps = {
   children: string;
-  href: string;
+  href?: string;
   color?: ColorOption;
   width?: WidthOption;
+  disabled?: boolean;
+  button?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-const Button = ({ children, href, color, width = "large" }: ButtonProps) => {
-  return (
+const Button = ({
+  children,
+  href,
+  color,
+  width = "large",
+  disabled,
+  button,
+  onClick,
+}: ButtonProps) => {
+  return button ? (
+    <button
+      className={`rounded-md py-3 px-4 font-semibold text-sm text-center whitespace-nowrap ${
+        disabled && "disabled:cursor-not-allowed disabled:bg-gray-400"
+      } ${width === "large" ? "w-full" : "w-fit"} ${
+        color !== "white" && "text-white"
+      } ${color === "white" && "bg-white ring-1 ring-gray-300 text-black"} ${
+        color === "green" && "bg-green-600"
+      } ${color === "red" && "bg-red-light"} ${color ? color : "bg-black"}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  ) : (
     <Link
-      href={href}
-      className={`rounded-md py-3 px-4 font-semibold text-sm text-center ${
+      href={href || ""}
+      className={`rounded-md py-3 px-4 font-semibold text-sm text-center whitespace-nowrap ${
         width === "large" ? "w-full" : "w-fit"
       } ${color !== "white" && "text-white"} ${
         color === "white" && "bg-white ring-1 ring-gray-300 text-black"
