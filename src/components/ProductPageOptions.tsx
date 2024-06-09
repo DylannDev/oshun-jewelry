9e8150a8172d830a7a1c06414314cbd02abd4c21
dylann97 <d.xavero@hotmail.com>
2024-06-09 05:26:53 +0200

    🚧  F04: App > Add useCartStore to manage the cart with zustand

--- src/components/ProductPageOptions.tsx
+++ src/components/ProductPageOptions.tsx
@@ -4,20 +4,43 @@ import Button from "./Button";
 import SizeOption from "./SizeOption";
 import QuantityOption from "./QuantityOption";
 import { products } from "@wix/stores";
+import useWixClient from "@/hooks/useWixClient";
 import { useState } from "react";
+import { useCartStore } from "@/hooks/useCartStore";
 
 type ProductOptionsProps = {
   product: products.Product;
 };
 
 const ProductPageOptions = ({ product }: ProductOptionsProps) => {
-  const [selectedOptions, setSelectedOptions] = useState("");
-  const [quantity, setQuantity] = useState(1);
+  const [selectedOptions, setSelectedOptions] = useState<string>("");
+  const [quantity, setQuantity] = useState<number>(1);
 
   const sizes = product.productOptions;
-  const productId = product._id;
+  const productId = product._id || "";
+  const variantId = "00000000-0000-0000-0000-000000000000";
   const stockNumber = product.stock?.quantity;
   const isInStock = product.stock?.inStock ? 10 : 0;
+
+  const wixClient = useWixClient();
+
+  // const { addItem } = useCartStore();
+
+  const addProduct = async () => {
+    const response = await wixClient.currentCart.addToCurrentCart({
+      lineItems: [
+        {
+          catalogReference: {
+            appId: process.env.NEXT_PUBLIC_WIX_APP_ID!,
+            catalogItemId: productId,
+            options: { variantId: variantId },
+          },
+          quantity: 1,
+        },
+      ],
+    });
+    console.log(response);
+  };
 
   return (
     <div className="flex flex-col gap-8">
@@ -38,9 +61,16 @@ const ProductPageOptions = ({ product }: ProductOptionsProps) => {
       )}
       <div className="flex flex-col gap-4">
         <Button href="/">Acheter</Button>
-        <Button href="/" color="white">
+        <Button color="white" onClick={addProduct} button>
           Ajouter au panier
         </Button>
+        {/* <Button
+          color="white"
+          onClick={() => addItem(wixClient, productId, variantId, quantity)}
+          button
+        >
+          Ajouter au panier
+        </Button> */}
       </div>
     </div>
   );
