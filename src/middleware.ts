8957a567a1320bf970c1ab00e42eb942e4c52d11
dylann97 <d.xavero@hotmail.com>
2024-06-19 12:14:22 +0200

    🔧  F05: middleware > Set the wix tokens config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 import { OAuthStrategy, createClient } from "@wix/sdk";
-import { WIX_REFRESH_TOKEN } from "./app/constants";
 
 export const middleware = async (request: NextRequest) => {
   const cookies = request.cookies;
   const res = NextResponse.next();
 
-  if (cookies.get(WIX_REFRESH_TOKEN)) {
+  if (cookies.get("refreshToken")) {
     return res;
   }
   const wixClient = createClient({
     auth: OAuthStrategy({ clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID! }),
   });
   const tokens = await wixClient.auth.generateVisitorTokens();
-  res.cookies.set(WIX_REFRESH_TOKEN, JSON.stringify(tokens.refreshToken), {
+  res.cookies.set("refreshToken", JSON.stringify(tokens.refreshToken), {
     maxAge: 60 * 60 * 24 * 30,
   });
   return res;
