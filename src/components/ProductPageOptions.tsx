33830f3f519ea50a30397f31f0f75d369172632f
dylann97 <d.xavero@hotmail.com>
2024-06-19 12:10:04 +0200

    ✨  F05: ProductPageOptions > Build the logic of adding a product to cart

--- src/components/ProductPageOptions.tsx
+++ src/components/ProductPageOptions.tsx
@@ -5,42 +5,60 @@ import SizeOption from "./SizeOption";
 import QuantityOption from "./QuantityOption";
 import { products } from "@wix/stores";
 import useWixClient from "@/hooks/useWixClient";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCartStore } from "@/hooks/useCartStore";
+import { findMatchingVariant } from "@/utils/array";
 
 type ProductOptionsProps = {
   product: products.Product;
 };
 
 const ProductPageOptions = ({ product }: ProductOptionsProps) => {
-  const [selectedOptions, setSelectedOptions] = useState<string>("");
-  const [quantity, setQuantity] = useState<number>(1);
+  const [selectedOptions, setSelectedOptions] = useState("");
+  const [variantId, setVariantId] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   const sizes = product.productOptions;
-  const productId = product._id || "";
-  const variantId = "00000000-0000-0000-0000-000000000000";
+  const productId = product._id!;
+  // const variantId = "00000000-0000-0000-0000-000000000000";
   const stockNumber = product.stock?.quantity;
   const isInStock = product.stock?.inStock ? 10 : 0;
 
   const wixClient = useWixClient();
 
-  // const { addItem } = useCartStore();
+  const { addItem } = useCartStore();
 
-  const addProduct = async () => {
-    const response = await wixClient.currentCart.addToCurrentCart({
-      lineItems: [
-        {
-          catalogReference: {
-            appId: process.env.NEXT_PUBLIC_WIX_APP_ID!,
-            catalogItemId: productId,
-            options: { variantId: variantId },
-          },
-          quantity: 1,
-        },
-      ],
-    });
-    console.log(response);
+  const clearCart = async () => {
+    try {
+      const currentCart = await wixClient.currentCart.getCurrentCart();
+      if (
+        currentCart &&
+        currentCart.lineItems &&
+        currentCart.lineItems.length > 0
+      ) {
+        await wixClient.currentCart.deleteCurrentCart();
+        console.log("Cart cleared");
+      } else {
+        console.log("No cart found or cart is already empty");
+      }
+    } catch (error: any) {
+      if (error.message.includes("OWNED_CART_NOT_FOUND")) {
+        console.log("No cart found for the current user");
+      } else {
+        console.error("An error occurred while clearing the cart:", error);
+      }
+    }
   };
+
+  useEffect(() => {
+    const selectedVariantId = findMatchingVariant(
+      selectedOptions,
+      product.variants || []
+    );
+    setVariantId(selectedVariantId || "");
+  }, [selectedOptions, product.variants]);
+
+  console.log("variantId", variantId);
 
   return (
     <div className="flex flex-col gap-8">
@@ -51,26 +69,26 @@ const ProductPageOptions = ({ product }: ProductOptionsProps) => {
           setSelectedOptions={setSelectedOptions}
         />
       )}
-      {productId && (
-        <QuantityOption
-          quantity={quantity}
-          setQuantity={setQuantity}
-          productId={productId}
-          stockNumber={stockNumber ? stockNumber : isInStock}
-        />
-      )}
+
+      <QuantityOption
+        quantity={quantity}
+        setQuantity={setQuantity}
+        stockNumber={stockNumber ? stockNumber : isInStock}
+      />
+
       <div className="flex flex-col gap-4">
-        <Button href="/">Acheter</Button>
-        <Button color="white" onClick={addProduct} button>
-          Ajouter au panier
+        <Button onClick={clearCart} button>
+          Vider cart
         </Button>
-        {/* <Button
+        <Button
           color="white"
-          onClick={() => addItem(wixClient, productId, variantId, quantity)}
+          onClick={() =>
+            addItem(wixClient, productId, variantId, selectedOptions, quantity)
+          }
           button
         >
           Ajouter au panier
-        </Button> */}
+        </Button>
       </div>
     </div>
   );
