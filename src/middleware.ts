d1de6cc355a6b82517d06bb3c2a109a7fbc0ccfd
dylann97 <d.xavero@hotmail.com>
2024-11-14 09:31:35 +0100

    📦 Update project configuration settings

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
 import { OAuthStrategy, createClient } from "@wix/sdk";
+import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = async (request: NextRequest) => {
   const cookies = request.cookies;
@@ -8,12 +8,15 @@ export const middleware = async (request: NextRequest) => {
   if (cookies.get("refreshToken")) {
     return res;
   }
+
   const wixClient = createClient({
     auth: OAuthStrategy({ clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID! }),
   });
+
   const tokens = await wixClient.auth.generateVisitorTokens();
   res.cookies.set("refreshToken", JSON.stringify(tokens.refreshToken), {
     maxAge: 60 * 60 * 24 * 30,
   });
+
   return res;
 };
